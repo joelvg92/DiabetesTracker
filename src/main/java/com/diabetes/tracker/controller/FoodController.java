1a4370070297c062e2933b93c941c6be566427c3
Joel Varghese <solidrock92@gmail.com>
2020-11-29 04:29:34 -0500

    Added food api

--- src/main/java/com/diabetes/tracker/controller/FoodController.java
+++ src/main/java/com/diabetes/tracker/controller/FoodController.java
@@ -1,21 +1,33 @@
 package com.diabetes.tracker.controller;
 
 import com.diabetes.tracker.Food.Food;
+import com.diabetes.tracker.Food.Foodv2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
+import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 
 @RestController
 public class FoodController {
     @Autowired
     Food food;
+
     @GetMapping("/food/find")
-    public ResponseEntity<List<String>> getMedicationName(@RequestParam String foodName) throws InterruptedException, ExecutionException {
-        return ResponseEntity.ok().body(food.getFoodInfo(foodName));
+    public Foodv2 getMedicationName(@RequestParam String foodName) throws InterruptedException, ExecutionException {
+        String uri = "https://trackapi.nutritionix.com/v2/search/instant?query="+foodName;
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("x-app-id","57812624");
+        headers.add("x-app-key","ae86094b6c0369265174939ca0cb74dc");
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+        return restTemplate.exchange(uri, HttpMethod.GET, entity, Foodv2.class).getBody();
+        //return ResponseEntity.ok().body(food.getFoodInfo(foodName));
     }
 }
